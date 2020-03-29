ef4ef72080a43f2eaf40e75c1ef68b7dec2de1f8
KaayGee <goodnesskelechi6@gmail.com>
2020-03-28 18:59:28 -0700

    add script to complete javascript task

---
+++ script.js
@@ -0,0 +1,11 @@
+const name = "Goodness Kelechi";
+console.log(name);
+
+const courses = ["html", "css", "javascript"];
+console.log(courses);
+
+for (let i=1; i < 200; i++){
+    if(i % 2 !==0){
+        console.log(i);
+    }
+}
